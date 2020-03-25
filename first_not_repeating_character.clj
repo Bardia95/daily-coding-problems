c28b379c87622813fda79f90ae7d546e4b6fdefc
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-25 02:07:31 -0400

    Fixed formatting

--- first_not_repeating_character.clj
+++ first_not_repeating_character.clj
@@ -1,5 +1,5 @@
 (defn first-not-repeating-character
   "String->String, returns first non-repeating-character of a string, if all repeating, returns \"_\""
   [s]
-  (let [ans (str (first ((group-by (frequencies s) s) 1)))] 
-    (if (= ans "") "_" ans)))
+  (let [res (str (first ((group-by (frequencies s) s) 1)))] 
+    (if (= res "") "_" res)))
