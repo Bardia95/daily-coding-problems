3aff7e6c2d4a9209710a9731d9fef12c376126ca
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-26 12:28:21 -0400

    Added century from year clojure solution

---
+++ code-signal-arcade-universe/clojure/century_from_year.clj
@@ -0,0 +1,11 @@
+(defn century-from-year
+  "Int->Int, Given a year, return the century it is in"
+  [y]
+  (if (= (mod y (f2d y)) 0)
+    (f2d y)
+    (+ 1 (f2d y))))
+
+(defn f2d
+  "Int->Int, Given a number 2+ digits long, return first two digits"
+  [n]
+  (Integer/parseInt (str (first (str n)) (second (str n)))))
