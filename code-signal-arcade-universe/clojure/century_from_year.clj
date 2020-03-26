ed89ba92ac89540b0330828e26531d7c6545f63c
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-26 12:30:43 -0400

    Fixed clojure century from year problem

--- code-signal-arcade-universe/clojure/century_from_year.clj
+++ code-signal-arcade-universe/clojure/century_from_year.clj
@@ -1,3 +1,8 @@
+(defn f2d
+  "Int->Int, Given a number 2+ digits long, return first two digits"
+  [n]
+  (Integer/parseInt (str (first (str n)) (second (str n)))))
+
 (defn century-from-year
   "Int->Int, Given a year, return the century it is in"
   [y]
@@ -5,7 +10,3 @@
     (f2d y)
     (+ 1 (f2d y))))
 
-(defn f2d
-  "Int->Int, Given a number 2+ digits long, return first two digits"
-  [n]
-  (Integer/parseInt (str (first (str n)) (second (str n)))))
