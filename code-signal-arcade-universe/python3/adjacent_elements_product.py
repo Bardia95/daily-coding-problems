9883b4bf30313104a0422b062deafec70eaecc42
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-26 14:27:48 -0400

    Added more elegant adjacent elements product python solution

--- code-signal-arcade-universe/python3/adjacent_elements_product.py
+++ code-signal-arcade-universe/python3/adjacent_elements_product.py
@@ -1,6 +1,19 @@
+# Original solution
+
 def adjacent_elements_product(l):
+    """
+    List->Integer, given a list, return the largest product of consecutive pairs
+    """
     res = float('-inf')
     for i in range(0, len(l) - 1):
         if (l[i] * l[i + 1] > res):
             res = l[i] * l[i + 1]
     return res
+
+# More elegant solution
+
+def max_product_pair(l):
+    """
+    List->Integer, given a list, return the largest product of consecutive pairs
+    """
+    return max([l[i] * l[i+1] for i in range(0, len(l)-1)])
