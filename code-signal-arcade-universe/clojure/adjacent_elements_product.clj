6e0c3a83aae7970e7678216c5abd9e728a770a94
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-26 14:17:11 -0400

    Added alternative adjacent elements product clojure solution

--- code-signal-arcade-universe/clojure/adjacent_elements_product.clj
+++ code-signal-arcade-universe/clojure/adjacent_elements_product.clj
@@ -1,5 +1,13 @@
+;; Original Solution
 (defn adjacent-elements-product
   "Vector->Integer, returns the largest product of consecutive pairs in a vector"
   [v]
   (apply max (for [i (range (- (count v) 1))]
                (* (v i) (v (+ i 1))))))
+
+;; Alternative solution
+
+(defn adjacent-elements-product
+  "Vector->Integer, returns the largest product of consecutive pairs in a vector"
+  [v]
+  (apply max (map #(apply * %) (partition 2 1 v))))
