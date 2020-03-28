e1a57567dd650eda73a3a70552fdb464d9fef0c1
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-27 22:43:40 -0400

    Added more elegant python solution for image rotation solution

--- code-signal-interview-prep/python3/rotate_image.py
+++ code-signal-interview-prep/python3/rotate_image.py
@@ -1,3 +1,5 @@
+# Original solution
+
 def rotate_image(l):
     """
     List->List, rotates a 2D matrix of any size 90 degrees clockwise
@@ -7,3 +9,11 @@ def rotate_image(l):
         for j in range(i):
             l[i][j], l[j][i] = l[j][i], l[i][j]
     return l
+
+# More elegant solution
+
+def rotate_matrix(m):
+    """
+    List->List, rotates a 2D matrix of any size 90 degrees clockwise
+    """
+    return list(zip(*m[::-1]))
