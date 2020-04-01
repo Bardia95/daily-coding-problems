9f35a5e1b467a27773df95685a215ce7bf93e512
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-31 22:08:55 -0400

    Added docstring to clojrue solution to all longest strings

--- code-signal-arcade-universe/clojure/all_longest_strings.clj
+++ code-signal-arcade-universe/clojure/all_longest_strings.clj
@@ -1,2 +1,4 @@
-(defn all-longest-strings [strings]
+(defn all-longest-strings
+  "List->List, given a list of strings, return all strings that have the longest length"
+  [strings]
   (remove #(< (count %) (count (last (sort-by count strings)))) strings))
