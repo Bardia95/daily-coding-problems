a60d0ff4048b4aa015c341516a1e2c9f627af7a7
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-03-31 22:15:31 -0400

    Added more elegant clojure solution to all longest strings

--- code-signal-arcade-universe/clojure/all_longest_strings.clj
+++ code-signal-arcade-universe/clojure/all_longest_strings.clj
@@ -2,3 +2,13 @@
   "List->List, given a list of strings, return all strings that have the longest length"
   [strings]
   (remove #(< (count %) (count (last (sort-by count strings)))) strings))
+
+
+(defn all-longest-strings-v2
+  "List->List, given a list of strings, return all strings that have the longest length"
+  [strings]
+  (->> strings
+       (group-by count)
+       (sort-by key >)
+       first
+       val))
