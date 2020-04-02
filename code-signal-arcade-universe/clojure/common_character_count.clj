1126bac7e343bbd6849bc3fc17ab815074cb5e66
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-01 22:06:59 -0400

    Added even more readable clojure solution to common character count w same logic

--- code-signal-arcade-universe/clojure/common_character_count.clj
+++ code-signal-arcade-universe/clojure/common_character_count.clj
@@ -1,4 +1,10 @@
 (defn common-character-count
   "String String -> Int, returns number characters in common between two strings"
   [s1 s2]
-  (reduce + (vals (select-keys (merge-with #(min %1 %2) (frequencies s1) (frequencies s2)) (distinct (filter (into  #{} s1) s2))))))
+  (let [f1 (frequencies s1)
+        f2 (frequencies s2)
+        mp (merge-with #(min %1 %2) f1 f2)
+        ks (distinct (filter (into #{} s1) s2))
+        vs (vals (select-keys mp ks))
+        rs (reduce + vs)]
+    rs))
