d7c6c379d12016ef74c7274656129868e5358104
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-06 22:02:02 -0400

    Added clojure solution for is_ipv4-address

---
+++ code-signal-arcade-universe/clojure/is_ipv4_address.clj
@@ -0,0 +1,42 @@
+(defn is-ipv4-address [s]
+  (try
+    (let [ip (map #(Integer/parseInt %) (clojure.string/split s #"\."))]
+      (and (= 4 (count ip))
+           (every? #(<= 0 % 255) ip)))
+    (catch Exception _ false)))
+
+
+(comment "
+An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+
+Given a string, find out if it satisfies the IPv4 address naming rules.
+
+Example
+
+For inputString = "172.16.254.1", the output should be
+isIPv4Address(inputString) = true;
+
+For inputString = "172.316.254.1", the output should be
+isIPv4Address(inputString) = false.
+
+316 is not in range [0, 255].
+
+For inputString = ".254.255.0", the output should be
+isIPv4Address(inputString) = false.
+
+There is no first number.
+
+Input/Output
+
+[execution time limit] 16 seconds (clj)
+
+[input] string inputString
+
+A string consisting of digits, full stops and lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 30.
+
+[output] boolean
+
+true if inputString satisfies the IPv4 address naming rules, false otherwise.")
