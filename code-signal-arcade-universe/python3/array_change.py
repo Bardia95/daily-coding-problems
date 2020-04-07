4bf10231a80e29e9e4c620f04f13630c26230c00
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-06 20:26:54 -0400

    Added python solution to array change

---
+++ code-signal-arcade-universe/python3/array_change.py
@@ -0,0 +1,34 @@
+def array_change(a):
+    counter = 0
+    for i in range(1, len(a)):
+        if a[i] <= a[i-1]:
+            diff = (a[i-1] - a[i] + 1)
+            a[i] += diff
+            counter += diff
+    return counter
+
+
+"""
+You are given an array of integers. On each move you are allowed to increase exactly one of its element by one.
+Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+Example
+
+For inputArray = [1, 1, 1], the output should be
+arrayChange(inputArray) = 3.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+Guaranteed constraints:
+3 ≤ inputArray.length ≤ 105,
+-105 ≤ inputArray[i] ≤ 105.
+
+[output] integer
+
+The minimal number of moves needed to obtain a strictly increasing sequence from inputArray.
+It's guaranteed that for the given test cases the answer always fits signed 32-bit integer type.
+"""
