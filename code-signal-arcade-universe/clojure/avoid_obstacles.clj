941f4e7344575802a3689394bae3d0bc684b4d06
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-07 02:58:38 -0400

    Added python solution for avoid obstacles

---
+++ code-signal-arcade-universe/clojure/avoid_obstacles.clj
@@ -0,0 +1,38 @@
+(defn avoid-obstacles [a]
+  (loop [c 2]
+    (if (> (first (sort (for [n a]
+                          (mod n c)))) 0)
+      c
+      (recur (inc c)))))
+
+
+
+(comment "
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+avoidObstacles(inputArray) = 4.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+Non-empty array of positive integers.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 1000,
+1 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The desired length.
+
+")
