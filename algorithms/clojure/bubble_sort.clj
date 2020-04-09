3db13096e81bbeaea618873db08064ed2327ebef
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-08 22:59:50 -0400

    Added assertions for clojure bubble sort

--- algorithms/clojure/bubble_sort.clj
+++ algorithms/clojure/bubble_sort.clj
@@ -14,3 +14,11 @@
     (if (= xs ys)
       xs
       (recur ys))))
+
+(assert (= (bubble-sort [3 2 1]) [1 2 3]) "Test 1 failed")
+(assert (= (bubble-sort [1 2 3]) [1 2 3]) "Test 2 failed")
+(assert (= (bubble-sort [3 3 3]) [3 3 3]) "Test 3 failed")
+(assert (= (bubble-sort [1]) [1]) "Test 4 failed")
+(assert (= (bubble-sort [-1]) [-1]) "Test 5 failed")
+(assert (= (bubble-sort [9 -1 0 -8 5]) [-8 -1 0 5 9]) "Test 6 failed")
+(assert (= (bubble-sort []) []) "Test 7 failed")
