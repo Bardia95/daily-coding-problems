a8a8a566e65d95a00f51133deb6718308e556597
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-09 14:34:53 -0400

    Added docstrings to clojure merge sort

--- algorithms/clojure/merge_sort.clj
+++ algorithms/clojure/merge_sort.clj
@@ -1,30 +1,35 @@
-(defn merge [l r]
-  (loop [res []
+(defn merge
+  "Vector Vector -> Vector, returns given vectors merged and sorted"
+  [l r]
+  (loop [v []
          i    0
          j    0]
-    (if (and (< i (count l)) (< j (count r)))
+    (if (and (< i (count l))
+             (< j (count r)))
       (if (< (l i) (r j))
-        (recur (conj res (l i))
+        (recur (conj v (l i))
                (inc i)
                j)
-        (recur (conj res (r j))
+        (recur (conj v (r j))
                i
                (inc j)))
       (if (< i (count l))
-        (recur (conj res (l i))
+        (recur (conj v (l i))
                (inc i)
                j)
         (if (< j (count r))
-          (recur (conj res (r j))
+          (recur (conj v (r j))
                  i
                  (inc j))
-          res)))))
+          v)))))
 
-(defn merge-sort [v]
+(defn merge-sort
+  "Vector -> Vector, returns a sorted version of the given vector"
+  [v]
   (if (< (count v) 2)
     v
-    (let [mid (quot (count v) 2)
+    (let [mid   (quot (count v) 2)
           split (split-at mid v)
-          left (merge-sort (first split))
-          right (merge-sort (peek split))]
+          left  (vec (merge-sort (first split)))
+          right (vec (merge-sort (peek split)))]
       (merge left right))))
