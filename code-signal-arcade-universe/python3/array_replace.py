99d0c1436f79e6f7fbb2eda72627b0030be0851c
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-10 18:58:44 -0400

    Added array replace python solution

---
+++ code-signal-arcade-universe/python3/array_replace.py
@@ -0,0 +1,37 @@
+def array_replace(L, e, s):
+    for i in range(len(L)):
+        if L[i] == e:
+            L[i] = s
+    return L
+
+
+"""
+Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+
+Example
+
+For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
+arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+Guaranteed constraints:
+0 ≤ inputArray.length ≤ 104,
+0 ≤ inputArray[i] ≤ 109.
+
+[input] integer elemToReplace
+
+Guaranteed constraints:
+0 ≤ elemToReplace ≤ 109.
+
+[input] integer substitutionElem
+
+Guaranteed constraints:
+0 ≤ substitutionElem ≤ 109.
+
+[output] array.integer
+"""
