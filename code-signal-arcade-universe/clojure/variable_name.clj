9d9f40c42de2f88f92bb45ca69717178bd3baed3
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-10 19:34:18 -0400

    Added more elegant clojure solution to variable name

--- code-signal-arcade-universe/clojure/variable_name.clj
+++ code-signal-arcade-universe/clojure/variable_name.clj
@@ -1,6 +1,10 @@
 (defn variable-name [name]
-  (if (or (Character/isLetter (first name)) (= \- (first name)))
+  (if (or (Character/isLetter (first name)) (= \_ (first name)))
     (->> (rest name)
-         (map #(or (Character/isLetter %) (Character/isDigit %) (= % \-)))
+         (map #(or (Character/isLetter %) (Character/isDigit %) (= % \_)))
          (every? true?))
     false))
+
+
+(defn variable-name-2 [name]
+  (not (nil? (re-matches #"[a-zA-Z_]\w+" name))))
