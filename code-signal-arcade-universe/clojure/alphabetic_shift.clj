ed3705bb187d9345575d3068381aec7e67092796
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-13 23:29:01 -0400

    Added more legible clojure solution to alphabetic shift with thread-first macro-based helper function

--- code-signal-arcade-universe/clojure/alphabetic_shift.clj
+++ code-signal-arcade-universe/clojure/alphabetic_shift.clj
@@ -2,6 +2,18 @@
   (apply str (map #(char (+ (mod (- (int %) 96) 26) 97)) s)))
 
 
+(defn shift [c]
+  (-> c
+      int
+      (- 96)
+      (mod 26)
+      (+ 97)
+      char))
+
+(defn alphabetic-shift-2 [s]
+  (apply str (map shift s)))
+
+
 (comment
   "Given a string, your task is to replace each of its characters by the next one in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
 
