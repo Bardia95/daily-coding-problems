6868999c9f79698e4ebd5ab41cb04d6f066798ff
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-24 14:46:27 -0400

    Added more elegant clojure knapsack light solution

--- code-signal-arcade-universe/clojure/knapsack_light.clj
+++ code-signal-arcade-universe/clojure/knapsack_light.clj
@@ -12,6 +12,18 @@
           0)))))
 
 
+(defn int-bool [bool]
+  (if bool
+    1
+    0))
+
+
+(defn knapsack-light [v1 w1 v2 w2 max-w]
+  (max (* (int-bool (<= (+ w1 w2) max-w)) (+ w1 w2))
+       (* (int-bool (<= w2 max-w)) v2)
+       (* (int-bool (<= w1 max-w)) v1)))
+
+
 (comment "You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 and is worth value2. What is the total maximum value of the items you can take with you, assuming that your max weight capacity is maxW and you can't come back for the items later?
 
 Note that there are only two items and you can't bring more than one item of each type, i.e. you can't take two first items or two second items.
