c40bca91c79587831e9b8bc64eccfeac191785c3
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-26 21:41:48 -0400

    Clojure Is Beautiful String Solution Updated

--- code-signal-arcade-universe/clojure/is_beautiful_string.clj
+++ code-signal-arcade-universe/clojure/is_beautiful_string.clj
@@ -1,1 +1,8 @@
-(defn is-beautiful-string [s])
+(defn is-beautiful-string [s]
+  (let [az (into {}
+                 (for [z (apply str (map char (range 97 123)))]
+                   [z 0]))]
+    (->> (frequencies s)
+         (merge-with + alpha)
+         vals
+         (apply >=))))
