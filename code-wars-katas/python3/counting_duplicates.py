056d87f0b0cbc856ad6f8146952d94945fd664ea
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-02 20:13:38 -0400

    Python counting duplicates solution

---
+++ code-wars-katas/python3/counting_duplicates.py
@@ -0,0 +1,31 @@
+def duplicate_count(s):
+  return len([c for c in set(s.lower()) if s.lower().count(c)>1])
+
+def duplicate_count_2(text):
+    freqs = {}
+    duplicates = 0
+    text = text.lower()
+    for char in text:
+        if char in freqs:
+            freqs[char] += 1
+        else: 
+            freqs[char] = 1
+    for freq in freqs:
+        if freqs[freq] > 1:
+            duplicates += 1
+    return duplicates
+
+
+"""
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+"""
