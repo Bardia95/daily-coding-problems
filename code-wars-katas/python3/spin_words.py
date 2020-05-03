3e166bb6ab7e8a3d88b3ec3215c330d135f6a195
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-02 20:10:06 -0400

    Python spin words kata alternative solution

--- code-wars-katas/python3/spin_words.py
+++ code-wars-katas/python3/spin_words.py
@@ -2,6 +2,16 @@ def spin_words(sentence):
     return ' '.join([x[::-1] if len(x) > 4 else x for x in sentence.split(' ')])
 
 
+def spin_words_2(sentence):
+    res = []
+    for word in sentence.split(' '):
+        if len(word) > 4:
+            res.append(word[::-1])
+        else:
+            res.append(word)
+    return ' '.join(res)
+
+
 """
 Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
