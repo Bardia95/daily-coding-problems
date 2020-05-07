7861287ea583a596ae3455b0e28a189d14b376ba
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-06 23:41:43 -0400

    Clojure longest word solution

---
+++ code-signal-arcade-universe/clojure/longest_word.clj
@@ -0,0 +1,23 @@
+(defn longest-word [s]
+  (last (sort-by count (re-seq #"\w+" s))))
+
+(comment "Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+
+Example
+
+For text = "Ready, steady, go!", the output should be
+longestWord(text) = "steady".
+
+Input/Output
+
+[execution time limit] 16 seconds (clj)
+
+[input] string text
+
+Guaranteed constraints:
+4 ≤ text.length ≤ 50.
+
+[output] string
+
+The longest word from text. It's guaranteed that there is a unique output.
+")
