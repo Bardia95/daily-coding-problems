c6850f20bae9960fe90e9cb2346f5d2db8d25dd3
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-12 22:22:00 -0400

    Python different squares solution

---
+++ code-signal-arcade-universe/python3/different_squares.py
@@ -0,0 +1,52 @@
+def different_squares(m):
+    s = set()
+    for i in range(len(m) - 1):
+        for j in range(len(m[i]) - 1):
+                s.add((m[i][j], m[i][j+1], m[i+1][j], m[i+1][j+1]))
+    return len(s)
+
+
+"""
+Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+
+Example
+
+For
+
+matrix = [[1, 2, 1],
+          [2, 2, 2],
+          [2, 2, 2],
+          [1, 2, 3],
+          [2, 2, 1]]
+the output should be
+differentSquares(matrix) = 6.
+
+Here are all 6 different 2 × 2 squares:
+
+1 2
+2 2
+2 1
+2 2
+2 2
+2 2
+2 2
+1 2
+2 2
+2 3
+2 3
+2 1
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.array.integer matrix
+
+Guaranteed constraints:
+1 ≤ matrix.length ≤ 100,
+1 ≤ matrix[i].length ≤ 100,
+0 ≤ matrix[i][j] ≤ 9.
+
+[output] integer
+
+The number of different 2 × 2 squares in matrix.
+"""
