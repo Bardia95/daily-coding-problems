7627b200cb0f8756c6b85e64d6e1063c59e29520
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-12 22:20:02 -0400

    Clojure different squares solution

--- code-signal-arcade-universe/clojure/different_squares.clj
+++ code-signal-arcade-universe/clojure/different_squares.clj
@@ -4,3 +4,47 @@
                                  k (range 2)
                                  l (range 2)]
                              (get-in matrix [(+ i k) (+ j l)]))))))
+
+
+(comment "Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+
+Example
+
+For
+
+matrix = [[1, 2, 1],
+          [2, 2, 2],
+          [2, 2, 2],
+          [1, 2, 3],
+          [2, 2, 1]]
+the output should be
+differentSquares(matrix) = 6.
+
+Here are all 6 different 2 × 2 squares:
+
+1 2
+2 2
+2 1
+2 2
+2 2
+2 2
+2 2
+1 2
+2 2
+2 3
+2 3
+2 1
+Input/Output
+
+[execution time limit] 16 seconds (clj)
+
+[input] array.array.integer matrix
+
+Guaranteed constraints:
+1 ≤ matrix.length ≤ 100,
+1 ≤ matrix[i].length ≤ 100,
+0 ≤ matrix[i][j] ≤ 9.
+
+[output] integer
+
+The number of different 2 × 2 squares in matrix.")
