4294d3efe461138935bcc96269cfca5d4c46326d
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-21 23:04:17 -0400

    Python digits product solution

---
+++ code-signal-arcade-universe/python3/digits_product.py
@@ -0,0 +1,27 @@
+import numpy
+
+def digits_product(p):
+    for i in range(1, 10000):
+        if p === numpy.prod([int(x) for x in str()])
+    return - 1
+"""
+Given an integer product, find the smallest positive (i.e. greater than 0) integer the product of whose digits is equal to product. If there is no such integer, return -1 instead.
+
+Example
+
+For product = 12, the output should be
+digitsProduct(product) = 26;
+For product = 19, the output should be
+digitsProduct(product) = -1.
+Input/Output
+
+[execution time limit] 16 seconds (clj)
+
+[input] integer product
+
+Guaranteed constraints:
+0 ≤ product ≤ 600.
+
+[output] integer
+
+"""
