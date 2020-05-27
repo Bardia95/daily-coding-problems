726477487179b457b33e0c8d2efc4c1e7b03826e
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-26 22:57:53 -0400

    Refactor binary->char-seq

--- code-signal-arcade-universe/clojure/message_from_binary_code.clj
+++ code-signal-arcade-universe/clojure/message_from_binary_code.clj
@@ -1,2 +1,8 @@
-(defn messageFromBinaryCode [code]
-  (apply str (map #(char (Integer/parseInt (clojure.string/join %) 2)) (partition 8 code))))
+(defn binary->string
+  [b]
+  (char (Integer/parseInt (clojure.string/join b) 2)))
+
+
+(defn messageFromBinaryCode
+  [code]
+  (apply str (map binary->string (partition 8 code))))
