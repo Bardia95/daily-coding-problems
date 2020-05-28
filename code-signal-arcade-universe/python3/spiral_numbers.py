1ea3e6c577962b20044b2859fcccb9182b836140
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-27 22:29:10 -0400

    Spiral numbers python solution

---
+++ code-signal-arcade-universe/python3/spiral_numbers.py
@@ -0,0 +1,36 @@
+def spiral_numbers(n):
+    m = [[0] * n for i in range(n)]
+    dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+    x, y, c = 0, -1, 1
+    for i in range(n + n - 1):
+        for j in range((n + n - i) // 2):
+            x += dx[i % 4]
+            y += dy[i % 4]
+            m[x][y] = c
+            c += 1
+    return m
+
+
+"""
+Construct a square matrix with a size N × N containing integers from 1 to N * N in a spiral order, starting from top-left and in clockwise direction.
+
+Example
+
+For n = 3, the output should be
+
+spiralNumbers(n) = [[1, 2, 3],
+                    [8, 9, 4],
+                    [7, 6, 5]]
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] integer n
+
+Matrix size, a positive integer.
+
+Guaranteed constraints:
+3 ≤ n ≤ 100.
+
+[output] array.array.integer
+"""
