2d50c577c987330d89e893ac0a96c8b481d46110
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-28 23:04:23 -0400

    Clojure sudoku solution

---
+++ code-signal-arcade-universe/clojure/sudoku.clj
@@ -0,0 +1,80 @@
+(defn r [grid i]
+  (not= (range 1 10) (sort (grid i))))
+
+(defn c [grid i]
+  (not= (range 1 10)
+        (sort (for [x (range 9)]
+                ((grid x) i)))))
+
+(defn g [grid x y]
+  (not= (range 1 10)
+        (sort (for [i (range x (+ x 3))
+                    j (range y (+ y 3))]
+                ((grid i) j)))))
+
+
+(defn sudoku [grid]
+  (loop [i 0]
+    (if (= i 9)
+      (if (some true? (vec (for [i (range 0 9 3)
+                                 j (range 0 9 3)]
+                             (g grid i j))))
+        false
+        true)
+      (if (or (r grid i) (c grid i))
+        false
+        (recur (inc i))))))
+
+
+(comment "
+Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid contains all of the digits from 1 to 9.
+
+This algorithm should check if the given grid of numbers represents a correct solution to Sudoku.
+
+Example
+
+For
+grid = [[1, 3, 2, 5, 4, 6, 9, 8, 7],
+        [4, 6, 5, 8, 7, 9, 3, 2, 1],
+        [7, 9, 8, 2, 1, 3, 6, 5, 4],
+        [9, 2, 1, 4, 3, 5, 8, 7, 6],
+        [3, 5, 4, 7, 6, 8, 2, 1, 9],
+        [6, 8, 7, 1, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 3, 6, 5, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+the output should be
+sudoku(grid) = true;
+
+For
+grid = [[1, 3, 2, 5, 4, 6, 9, 2, 7],
+        [4, 6, 5, 8, 7, 9, 3, 8, 1],
+        [7, 9, 8, 2, 1, 3, 6, 5, 4],
+        [9, 2, 1, 4, 3, 5, 8, 7, 6],
+        [3, 5, 4, 7, 6, 8, 2, 1, 9],
+        [6, 8, 7, 1, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 3, 6, 5, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+the output should be
+sudoku(grid) = false.
+
+The output should be false: each of the nine 3 × 3 sub-grids should contain all of the digits from 1 to 9.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.array.integer grid
+
+A matrix representing 9 × 9 grid already filled with numbers from 1 to 9.
+
+Guaranteed constraints:
+grid.length = 9,
+grid[i].length = 9,
+1 ≤ grid[i][j] ≤ 9.
+
+[output] boolean
+
+true if the given grid represents a correct solution to Sudoku, false otherwise.
+")
